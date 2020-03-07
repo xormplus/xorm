c3c128a21ec06c8aab3632bf9a0a3c080b31f6e4
Unknown <xormplus@163.com>
2020-03-07 16:01:52 +0800

    Fixed Join strings on func Exist

--- session_exist_test.go
+++ session_exist_test.go
@@ -74,3 +74,106 @@ func TestExistStruct(t *testing.T) {
 	assert.NoError(t, err)
 	assert.False(t, has)
 }
+
+func TestExistStructForJoin(t *testing.T) {
+	assert.NoError(t, prepareEngine())
+
+	type Number struct {
+		Id  int64
+		Lid int64
+	}
+
+	type OrderList struct {
+		Id  int64
+		Eid int64
+	}
+
+	type Player struct {
+		Id   int64
+		Name string
+	}
+
+	assert.NoError(t, testEngine.Sync2(new(Number), new(OrderList), new(Player)))
+
+	var ply Player
+	cnt, err := testEngine.Insert(&ply)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, cnt)
+
+	var orderlist = OrderList{
+		Eid: ply.Id,
+	}
+	cnt, err = testEngine.Insert(&orderlist)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, cnt)
+
+	var um = Number{
+		Lid: orderlist.Id,
+	}
+	cnt, err = testEngine.Insert(&um)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, cnt)
+
+	session := testEngine.NewSession()
+	defer session.Close()
+
+	session.Table("number").
+		Join("INNER", "order_list", "order_list.id = number.lid").
+		Join("LEFT", "player", "player.id = order_list.eid").
+		Where("number.lid = ?", 1)
+	has, err := session.Exist()
+	assert.NoError(t, err)
+	assert.True(t, has)
+
+	session.Table("number").
+		Join("INNER", "order_list", "order_list.id = number.lid").
+		Join("LEFT", "player", "player.id = order_list.eid").
+		Where("number.lid = ?", 2)
+	has, err = session.Exist()
+	assert.NoError(t, err)
+	assert.False(t, has)
+
+	session.Table("number").
+		Select("order_list.id").
+		Join("INNER", "order_list", "order_list.id = number.lid").
+		Join("LEFT", "player", "player.id = order_list.eid").
+		Where("order_list.id = ?", 1)
+	has, err = session.Exist()
+	assert.NoError(t, err)
+	assert.True(t, has)
+
+	session.Table("number").
+		Select("player.id").
+		Join("INNER", "order_list", "order_list.id = number.lid").
+		Join("LEFT", "player", "player.id = order_list.eid").
+		Where("player.id = ?", 2)
+	has, err = session.Exist()
+	assert.NoError(t, err)
+	assert.False(t, has)
+
+	session.Table("number").
+		Select("player.id").
+		Join("INNER", "order_list", "order_list.id = number.lid").
+		Join("LEFT", "player", "player.id = order_list.eid")
+	has, err = session.Exist()
+	assert.NoError(t, err)
+	assert.True(t, has)
+
+	err = session.DropTable("order_list")
+	assert.NoError(t, err)
+
+	session.Table("number").
+		Select("player.id").
+		Join("INNER", "order_list", "order_list.id = number.lid").
+		Join("LEFT", "player", "player.id = order_list.eid")
+	has, err = session.Exist()
+	assert.Error(t, err)
+	assert.False(t, has)
+
+	session.Table("number").
+		Select("player.id").
+		Join("LEFT", "player", "player.id = number.lid")
+	has, err = session.Exist()
+	assert.NoError(t, err)
+	assert.True(t, has)
+}
